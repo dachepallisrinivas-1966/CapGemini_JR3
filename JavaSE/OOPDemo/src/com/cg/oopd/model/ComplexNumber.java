65f9680a1e90124292ffbc19359777dbea569af2
dachepalli srinivas <dachepallisrinivas@gmail.com>
2021-02-11 15:30:19 +0530

    Day4

--- JavaSE/OOPDemo/src/com/cg/oopd/model/ComplexNumber.java
+++ JavaSE/OOPDemo/src/com/cg/oopd/model/ComplexNumber.java
@@ -5,26 +5,25 @@ public class ComplexNumber {
 	private double realPart;		// represent real part of the complex number
 	private double imaginaryPart;	// represent imaginary part 
 	
-	
 	/* default constructor */
 	public ComplexNumber() {
 		realPart = -1;
 		imaginaryPart = -1;
-		System.out.println("default constructor is called ");
+		// System.out.println("default constructor is called ");
 	}
 	
 	/* parameter constructor */
 	public ComplexNumber(double realPart) {
 		this.realPart = realPart;
 		this.imaginaryPart = 0;
-		System.out.println("one-parameter constructor is called ");
+		// System.out.println("one-parameter constructor is called ");
 	}
 	
 	/* parameter constructor */
 	public ComplexNumber(double realPart, double imaginaryPart) {
 		this.realPart = realPart;
 		this.imaginaryPart = imaginaryPart;
-		System.out.println("two-parameter constructor is called ");
+		// System.out.println("two-parameter constructor is called ");
 	}
 	
 	/* copy constructor */
@@ -54,6 +53,38 @@ public class ComplexNumber {
 	public double getImaginaryPart() {
 		return imaginaryPart;
 	}
+	
+	
+//	public ComplexNumber add(ComplexNumber other) {
+//		ComplexNumber result = new ComplexNumber();
+//		result.realPart = this.realPart + other.realPart;
+//		result.imaginaryPart = this.imaginaryPart + other.imaginaryPart;
+//		return result;
+//	}
+//	
+//	public ComplexNumber diff(ComplexNumber other) {
+//		ComplexNumber result = new ComplexNumber();
+//		result.realPart = this.realPart - other.realPart;
+//		result.imaginaryPart = this.imaginaryPart - other.imaginaryPart;
+//		return result;
+//	}
+	
+	public static ComplexNumber add(ComplexNumber first, ComplexNumber second) {
+		ComplexNumber result = new ComplexNumber();
+		result.realPart = first.realPart + second.realPart;
+		result.imaginaryPart = first.imaginaryPart + second.imaginaryPart;
+		return result;
+	}
+	
+	public static ComplexNumber diff(ComplexNumber first, ComplexNumber second) {
+		ComplexNumber result = new ComplexNumber();
+		result.realPart = first.realPart - second.realPart;
+		result.imaginaryPart = first.imaginaryPart - second.imaginaryPart;
+		return result;
+	}
+	
+	
+	
 	
 }
 
--- JavaSE/OOPDemo/src/com/cg/oopd/ui/ComplexNumberApp.java
+++ JavaSE/OOPDemo/src/com/cg/oopd/ui/ComplexNumberApp.java
@@ -5,61 +5,67 @@ import com.cg.oopd.model.ComplexNumber;
 public class ComplexNumberApp {
 
 	public static void main(String[] args) {
-		ComplexNumber c1 = new ComplexNumber();
-		System.out.println(c1.getRealPart());
-		System.out.println(c1.getImaginaryPart());
 		
-		ComplexNumber c2 = new ComplexNumber();
-		System.out.println(c2.getRealPart());
-		System.out.println(c2.getImaginaryPart());
+		ComplexNumber c1 = new ComplexNumber(10, 9);
+		ComplexNumber c2 = new ComplexNumber(8,-6);
 		
-		ComplexNumber c3 = new ComplexNumber(5);
-		System.out.println(c3.getRealPart());
-		System.out.println(c3.getImaginaryPart());
+		// ComplexNumber c3 = c1.add(c2);
+		ComplexNumber c3 = ComplexNumber.add(c1, c2);
+		System.out.println(c3.getRealPart() + ", " + c3.getImaginaryPart());
 		
-		ComplexNumber c4 = new ComplexNumber(4,7);
-		System.out.println(c4.getRealPart());
-		System.out.println(c4.getImaginaryPart());
+		// ComplexNumber c4 = c1.diff(c2);
+		ComplexNumber c4 = ComplexNumber.diff(c1, c2);
+		System.out.println(c4.getRealPart() + ", " + c4.getImaginaryPart());
 		
-		ComplexNumber c5 = c4;		// object initialization statement - no new object is created.
-		System.out.println(c5.getRealPart());
-		System.out.println(c5.getImaginaryPart());
-		
-		System.out.println("----------------------------");
-		
-		c5.setRealPart(5);
-		c5.setImaginaryPart(8);
-		System.out.println(c5.getRealPart());
-		System.out.println(c5.getImaginaryPart());
-		
-		System.out.println("----------------------------");
-		
-		System.out.println(c4.getRealPart());
-		System.out.println(c4.getImaginaryPart());
-		
-		System.out.println("----------------------------");
-		
-		ComplexNumber c6 = new ComplexNumber(c4);	// copy constructor is called
-		System.out.println(c6.getRealPart());
-		System.out.println(c6.getImaginaryPart());
-		
-		c4.setRealPart(40);
-		c4.setImaginaryPart(70);
-		
-		System.out.println("----------------------------");
-		
-		System.out.println(c4.getRealPart());
-		System.out.println(c4.getImaginaryPart());
-		
-		System.out.println(c6.getRealPart());
-		System.out.println(c6.getImaginaryPart());
-		
-		
-		
-		
-		
-		
-	
+//		ComplexNumber c1 = new ComplexNumber();
+//
+//		System.out.println(c1.getRealPart());
+//		System.out.println(c1.getImaginaryPart());
+//		
+//		ComplexNumber c2 = new ComplexNumber();
+//		System.out.println(c2.getRealPart());
+//		System.out.println(c2.getImaginaryPart());
+//		
+//		ComplexNumber c3 = new ComplexNumber(5);
+//		System.out.println(c3.getRealPart());
+//		System.out.println(c3.getImaginaryPart());
+//		
+//		ComplexNumber c4 = new ComplexNumber(4,7);
+//		System.out.println(c4.getRealPart());
+//		System.out.println(c4.getImaginaryPart());
+//		
+//		ComplexNumber c5 = c4;		// object initialization statement - no new object is created.
+//		System.out.println(c5.getRealPart());
+//		System.out.println(c5.getImaginaryPart());
+//		
+//		System.out.println("----------------------------");
+//		
+//		c5.setRealPart(5);
+//		c5.setImaginaryPart(8);
+//		System.out.println(c5.getRealPart());
+//		System.out.println(c5.getImaginaryPart());
+//		
+//		System.out.println("----------------------------");
+//		
+//		System.out.println(c4.getRealPart());
+//		System.out.println(c4.getImaginaryPart());
+//		
+//		System.out.println("----------------------------");
+//		
+//		ComplexNumber c6 = new ComplexNumber(c4);	// copy constructor is called
+//		System.out.println(c6.getRealPart());
+//		System.out.println(c6.getImaginaryPart());
+//		
+//		c4.setRealPart(40);
+//		c4.setImaginaryPart(70);
+//		
+//		System.out.println("----------------------------");
+//		
+//		System.out.println(c4.getRealPart());
+//		System.out.println(c4.getImaginaryPart());
+//		
+//		System.out.println(c6.getRealPart());
+//		System.out.println(c6.getImaginaryPart());
 		
 //		c1.setRealPart(5);
 //		c1.setImaginaryPart(6);
