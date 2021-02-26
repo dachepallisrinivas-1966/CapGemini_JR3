c291b4360cf710dc216441dd1930d2a726b3a067
dachepalli srinivas <dachepallisrinivas@gmail.com>
2021-02-26 14:59:17 +0530

    Day 15

--- JavaSE2/JDBCDemo/src/main/java/com/cg/jd/ui/CallableDemo.java
+++ JavaSE2/JDBCDemo/src/main/java/com/cg/jd/ui/CallableDemo.java
@@ -12,12 +12,14 @@ public class CallableDemo {
 		try (Connection con = 
 				DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "sa")) {
 			
-			String sql = "{? = call 'SELECT NOW()'}";
-			CallableStatement cs = con.prepareCall(sql);
-			cs.execute();
+			CallableStatement cs = con.prepareCall("{? = call UPPER( ? )}");
 			cs.registerOutParameter(1, Types.VARCHAR);
+			cs.setString(2, "Srinivas Dachepalli");
+			cs.execute();
+			
 			String output = cs.getString(1);
 			System.out.println(output);
+			cs.close();
 			
 		} catch(SQLException excep) {
 			System.out.println(excep.getMessage());
